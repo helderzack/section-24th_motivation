0e2106a8cbfc133fc559b42ee1ae78346736ae13
helderzack <helderzacarias@outlook.com>
2023-09-07 20:54:35 -0300

    - Added custom toolbar;   - Implemented dummy actions on the icons inside the toolbar.

--- app/src/main/java/com/helder/section24_motivation/UserActivity.kt
+++ app/src/main/java/com/helder/section24_motivation/UserActivity.kt
@@ -1,6 +1,7 @@
 package com.helder.section24_motivation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.helder.section24_motivation.databinding.ActivityUserBinding
 
@@ -11,11 +12,20 @@ class UserActivity : AppCompatActivity() {
         binding = ActivityUserBinding.inflate(layoutInflater)
 
         with(binding) {
-            setSupportActionBar(toolbar)
+            imageAllPhrasesAction.setOnClickListener {
+                Toast.makeText(applicationContext, "Clicked on All", Toast.LENGTH_SHORT).show()
+            }
+
+            imageHappyPhrasesAction.setOnClickListener {
+                Toast.makeText(applicationContext, "Clicked on Happy", Toast.LENGTH_SHORT).show()
+            }
+
+            imageSunnyPhrasesAction.setOnClickListener {
+                Toast.makeText(applicationContext, "Clicked on Sunny", Toast.LENGTH_SHORT).show()
+            }
 
             val userName = intent.getStringExtra("userName")!!
-            textHelloUser.text = getString(R.string.user_greeeting, userName)
-
+            textHelloUser.text = getString(R.string.user_greeting, userName)
             setContentView(root)
         }
     }
